a770dfbf1990813aacac7c1a781fac4b8e82e9cf
Diego Rossi <diego.rossi@yahoo.com.br>
2025-11-03 21:00:58 -0300

    Aula 24_10

--- agenda-tarefa/src/components/categoriaTable.vue
+++ agenda-tarefa/src/components/categoriaTable.vue
@@ -8,6 +8,16 @@
     onMounted(() =>{
         categoriaStore.exibir();
     });
+
+    const colunas = [{
+        "title":"Nome",
+        "key":"nome"
+    },{
+        "title":"Cor",
+        "key":"cor"
+    }, {
+        "title":"",
+        "key":"actions" }];
 </script>
 
 <template>
@@ -16,7 +26,12 @@
             <v-card-title>Lista de categorias</v-card-title>
             <v-card-text>
                 <v-data-table 
-                    :items="categoriaStore.categorias">
+                    :items="categoriaStore.categorias"
+                    :headers="colunas">
+                    <template v-slot:item.actions="{ item }">
+                        <v-btn @click="alterar(item)">Alterar</v-btn>
+                        <v-btn @click="excluir(item)">Excluir</v-btn>
+                    </template>
                 </v-data-table>
             </v-card-text>
         </v-card>
